6c213678e14bd82b8b6424f17fda0d4234bcfb4a
maciej.ksiazek <maciej.ksiazek@6a13c4b1-f019-0410-8bd6-1f74a21916ef>
2006-09-24 11:29:37 +0000

    moving project from trunk/tt to trunk. Step 2 Please checkout once again directly from trunk (NOT from trunk/tt)

--- tt/db/migrate/003_add_salt_field_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddSaltFieldToUsers < ActiveRecord::Migration
-  def self.up
-    execute "ALTER TABLE `users` ADD `salt` VARCHAR( 255 ) NOT NULL"
-  end
-
-  def self.down
-    execute "ALTER TABLE `users` DROP `salt` "
-  end
-end
--- tt/db/migrate/007_add_inactive_to_clients.rb
+++
@@ -1,9 +0,0 @@
-class AddInactiveToClients < ActiveRecord::Migration
-  def self.up
-    execute "ALTER TABLE `clients` ADD COLUMN `is_inactive` BOOLEAN NOT NULL DEFAULT 0;"
-  end
-
-  def self.down
-    execute "ALTER TABLE `clients` DROP COLUMN `is_inactive`"
-  end
-end
