9c6db8a788daf986355c06656df40f2b6ac552e8
maciej.ksiazek <maciej.ksiazek@6a13c4b1-f019-0410-8bd6-1f74a21916ef>
2006-09-24 11:29:37 +0000

    moving project from trunk/tt to trunk. Step 2 Please checkout once again directly from trunk (NOT from trunk/tt)

--- trunk/tt/db/migrate/004_add_is_inactive_field_to_projects.rb
+++
@@ -1,9 +0,0 @@
-class AddIsInactiveFieldToProjects < ActiveRecord::Migration
-  def self.up
-    execute "ALTER TABLE `projects` ADD `is_inactive` BOOL NOT NULL DEFAULT '0'"
-  end
-
-  def self.down
-    execute "ALTER TABLE `projects` DROP `is_inactive` "
-  end
-end
