5948a0aa103bdabb53a0164ab915d01657af8162
apohllo <apohllo@6a13c4b1-f019-0410-8bd6-1f74a21916ef>
2007-02-06 20:06:15 +0000

    restricted validation

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -4,7 +4,7 @@ class Invoice < ActiveRecord::Base
   has_many :activities
   has_many :projects, :through => :activity
   
-  validates_presence_of :client_id, :name
+  validates_presence_of :client_id, :name, :created_at, :user_id
   validates_uniqueness_of :name
   validates_length_of(:name, :within => 2..20)
   
