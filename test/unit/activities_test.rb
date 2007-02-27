5030055d4ce64c2cfe0cbd96aa8703d4beb5ea45
maciej.ksiazek <maciej.ksiazek@6a13c4b1-f019-0410-8bd6-1f74a21916ef>
2007-02-27 23:00:15 +0000

    removing temporary test method from activities_test.rb

--- test/unit/activities_test.rb
+++ test/unit/activities_test.rb
@@ -4,15 +4,9 @@ class ActivityTest < Test::Unit::TestCase
   fixtures :activities
 
   def setup
-    @activity = Activity.find(1)
-    @activities = Activity.find((1..3).to_a)
+    @activity = Activity.find :first
   end
 
-  #test activities ordering
-  def test_order
-    assert_equal 3, @activities.size 
-  end
-  
   def test_convert_duration
     assert_equal 60, Activity.convert_duration("1:00")
     assert_equal 10, Activity.convert_duration("0:10")
