16b5694b657fde8880f71fc2cb64d9c6f7175c4b
jakub.suder <jakub.suder@6a13c4b1-f019-0410-8bd6-1f74a21916ef>
2008-02-22 14:16:12 +0000

    fixed activity search by date_from and date_to, which had stopped working when javascript calendar was added; added missing role column in csv report

--- trunk/lib/params_filter.rb
+++ trunk/lib/params_filter.rb
@@ -1,26 +1,11 @@
 module ParamsFilter
 
   # Prepares params[:search] to be passed directly to model methods
+  private
   def prepare_search_dates
-    params[:search]||= {}
+    params[:search] ||= {}
     params[:search][:default_month] = session[:month]
     params[:search][:default_year]  = session[:year]
-        
-    # Fill start date if selected on form
-    if params[:dates] and not params[:dates][:date_from].blank?
-      params[:search][:date_from] = params[:dates]["date_from(1i)"].to_i.to_s \
-          + "-" + params[:dates]["date_from(2i)"].to_i.to_s \
-          + "-" + params[:dates]["date_from(3i)"].to_i.to_s
-    end
-  
-    # Fill end date if selected on form
-    if params[:dates] and not params[:dates]['date_to(1i)'].blank?
-      params[:search][:date_to] = params[:dates]["date_to(1i)"].to_i.to_s \
-                + "-" + params[:dates]["date_to(2i)"].to_i.to_s \
-                + "-" + params[:dates]["date_to(3i)"].to_i.to_s          
-    end
   end
-  private :prepare_search_dates
-  
-  
+
 end
