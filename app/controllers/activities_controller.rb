c119f62d86d11f4d935fc9f206cdfa5061e78bd6
jakub.suder <jakub.suder@6a13c4b1-f019-0410-8bd6-1f74a21916ef>
2008-02-22 14:16:12 +0000

    fixed activity search by date_from and date_to, which had stopped working when javascript calendar was added; added missing role column in csv report

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -61,21 +61,21 @@ class ActivitiesController < ApplicationController
   #
   # Lists all current activities or specified by search conditions.
   #
-  def list 
-     if(params[:search].nil? and session[:month].nil? and session[:year].nil?)
-        # Return all activities, paginated
-        @activities = Activity.list( {}, {:page=> params[:page]})
-     else
-        # Prepare params[:search] to be passed directly to Activity.list
-        prepare_search_dates      
-        
-        # Get client and his not issued invoices if client is selected
-        @client_id = Project.find(params[:search][:project_id]).client_id unless params[:search][:project_id].blank?
-        @invoices = Invoice.find(:all, :conditions => ["client_id = ? AND is_issued=0", @client_id], :order => "created_at DESC")
-    
-        # Get list of activities meeting specified conditions
-        @activities = Activity.list( params[:search] )
-     end
+  def list
+    if params[:search] or session[:month] or session[:year]
+      # Prepare params[:search] to be passed directly to Activity.list
+      prepare_search_dates
+
+      # Get client and his not issued invoices if client is selected
+      @client_id = Project.find(params[:search][:project_id]).client_id unless params[:search][:project_id].blank?
+      @invoices = Invoice.find(:all, :conditions => ["client_id = ? AND is_issued=0", @client_id], :order => "created_at DESC")
+
+      # Get list of activities meeting specified conditions
+      @activities = Activity.list(params[:search])
+    else
+      # Return all activities, paginated
+      @activities = Activity.list({}, {:page => params[:page]})
+    end
   end
     
   #
@@ -179,7 +179,7 @@ class ActivitiesController < ApplicationController
       csv << header
       activities.each do |activity|
         minutes += activity.minutes
-	      data = [activity.project.name, activity.user.login, activity.date, activity.minutes]
+	      data = [activity.project.name, activity.user.login, activity.user.role.name, activity.date, activity.minutes]
 	      data << activity.comments if params[:search] and params[:search][:details]
         csv << data 
       end
